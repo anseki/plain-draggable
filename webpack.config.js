a9b867a15afdc13b71c4e20e27c1b4a9e6cc4f87
anseki <AnSeki.aff@gmail.com>
2017-08-14 20:40:01 +0900

    Separate `webpack.config.rules.js` to be imported.

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,24 @@ const
   BASE_NAME = 'plain-draggable',
   OBJECT_NAME = 'PlainDraggable',
 
-  IMPORTED_PACKAGES_PATH = [
+  webpack = require('webpack'),
+  path = require('path'),
+  PKG = require('./package'),
+
+  RULES = require('./webpack.config.rules.js').concat([ // Join `webpack.config.rules.js` files
     'cssprefix',
     'anim-event',
     'm-class-list'
-  ].map(packageName => require.resolve(packageName) // Get package root path
-    .replace(new RegExp(`^(.*[/\\\\]node_modules[/\\\\]${packageName}[/\\\\]).*$`), '$1')),
-
-  LIMIT_TAGS = ['SNAP', 'SVG', 'LEFTTOP'],
-
-  webpack = require('webpack'),
-  path = require('path'),
-  preProc = require('pre-proc'),
-  PKG = require('./package'),
+  ].reduce((rules, packageName) =>
+    rules.concat(require(`./node_modules/${packageName}/webpack.config.rules.js`)), [])),
 
   BUILD = process.env.NODE_ENV === 'production',
   LIMIT = process.env.EDITION === 'limit',
-  SRC = process.env.SRC === 'yes',
 
   SRC_PATH = path.resolve(__dirname, 'src'),
   ENTRY_PATH = path.resolve(SRC_PATH, `${BASE_NAME}.js`),
   BUILD_PATH = BUILD ? __dirname : path.resolve(__dirname, 'test'),
-  BUILD_FILE = `${BASE_NAME}${LIMIT ? '-limit' : ''}${BUILD ? '.min' : ''}.js`,
-
-  BABEL_RULE = {
-    loader: 'babel-loader',
-    options: {
-      presets: ['es2015'],
-      plugins: ['add-module-exports']
-    }
-  };
-
-if (!LIMIT && SRC) { throw new Error('This options break source file.'); }
+  BUILD_FILE = `${BASE_NAME}${LIMIT ? '-limit' : ''}${BUILD ? '.min' : ''}.js`;
 
 module.exports = {
   entry: ENTRY_PATH,
@@ -48,37 +34,7 @@ module.exports = {
     libraryTarget: 'var'
   },
   resolve: {mainFields: ['jsnext:main', 'browser', 'module', 'main']},
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: absPath => !IMPORTED_PACKAGES_PATH.find(packagePath => absPath.indexOf(packagePath) === 0) &&
-          absPath.split(path.sep).includes('node_modules'),
-        use: [
-          BABEL_RULE,
-          {
-            loader: 'skeleton-loader',
-            options: {
-              procedure: function(content) {
-                if (LIMIT) {
-                  content = preProc.removeTag(LIMIT_TAGS, content, this.resourcePath, SRC_PATH);
-                  if (!BUILD && SRC && this.resourcePath === ENTRY_PATH) {
-                    // Save the source code of limited function, to check.
-                    const destPath = path.resolve(SRC_PATH, BUILD_FILE);
-                    require('fs').writeFileSync(destPath, content);
-                    console.log(`Output: ${destPath}`);
-                  }
-                }
-                return BUILD ?
-                  preProc.removeTag('DEBUG', content, this.resourcePath, IMPORTED_PACKAGES_PATH.concat(SRC_PATH)) :
-                  content;
-              }
-            }
-          }
-        ]
-      }
-    ]
-  },
+  module: {rules: RULES},
   devtool: BUILD ? false : 'source-map',
   plugins: BUILD ? [
     new webpack.optimize.UglifyJsPlugin({compress: {warnings: true}}),
---
+++ webpack.config.rules.js
@@ -0,0 +1,53 @@
+/* eslint-env node, es6 */
+
+'use strict';
+
+const
+  path = require('path'),
+  SRC_PATH = path.resolve(__dirname, 'src'),
+  BUILD = process.env.NODE_ENV === 'production',
+  LIMIT = process.env.EDITION === 'limit',
+  SRC = process.env.SRC === 'yes',
+  BABEL_RULE = {
+    loader: 'babel-loader',
+    options: {
+      presets: ['es2015'],
+      plugins: ['add-module-exports']
+    }
+  },
+
+  LIMIT_TAGS = ['SNAP', 'SVG', 'LEFTTOP'],
+  BASE_NAME = 'plain-draggable',
+  ENTRY_PATH = path.resolve(SRC_PATH, `${BASE_NAME}.js`),
+  BUILD_FILE = `${BASE_NAME}${LIMIT ? '-limit' : ''}${BUILD ? '.min' : ''}.js`,
+  preProc = require('pre-proc');
+
+if (!LIMIT && SRC) { throw new Error('This options break source file.'); }
+
+module.exports = [
+  {
+    resource: {and: [SRC_PATH, /\.js$/]},
+    use: [
+      BABEL_RULE,
+      {
+        loader: 'skeleton-loader',
+        options: {
+          procedure: function(content) {
+            if (LIMIT) {
+              content = preProc.removeTag(LIMIT_TAGS, content, this.resourcePath, SRC_PATH);
+              if (!BUILD && SRC && this.resourcePath === ENTRY_PATH) {
+                // Save the source code of limited function, to check.
+                const destPath = path.resolve(SRC_PATH, BUILD_FILE);
+                require('fs').writeFileSync(destPath, content);
+                console.log(`Output: ${destPath}`);
+              }
+            }
+            return BUILD ?
+              preProc.removeTag('DEBUG', content, this.resourcePath, SRC_PATH) :
+              content;
+          }
+        }
+      }
+    ]
+  }
+];
