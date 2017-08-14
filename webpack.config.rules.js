d65b2fbf4a5d91a82f4ae62cfcebdd1e44752bae
anseki <AnSeki.aff@gmail.com>
2017-08-14 21:08:26 +0900

    Fix: remove checking path in `preProc.removeTag`

--- webpack.config.rules.js
+++ webpack.config.rules.js
@@ -34,7 +34,7 @@ module.exports = [
         options: {
           procedure: function(content) {
             if (LIMIT) {
-              content = preProc.removeTag(LIMIT_TAGS, content, this.resourcePath, SRC_PATH);
+              content = preProc.removeTag(LIMIT_TAGS, content);
               if (!BUILD && SRC && this.resourcePath === ENTRY_PATH) {
                 // Save the source code of limited function, to check.
                 const destPath = path.resolve(SRC_PATH, BUILD_FILE);
@@ -42,9 +42,7 @@ module.exports = [
                 console.log(`Output: ${destPath}`);
               }
             }
-            return BUILD ?
-              preProc.removeTag('DEBUG', content, this.resourcePath, SRC_PATH) :
-              content;
+            return BUILD ? preProc.removeTag('DEBUG', content) : content;
           }
         }
       }
