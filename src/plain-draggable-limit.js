1b59560d6b8b5c40af4ca4492f1a77ea49d6ca13
anseki <AnSeki.aff@gmail.com>
2017-09-14 18:58:55 +0900

    Update build script

--- src/plain-draggable-limit.js
+++ src/plain-draggable-limit.js
@@ -1,4 +1,8 @@
 /*
+  DON'T MANUALLY EDIT THIS FILE; run `npm run dev-limit` instead.
+*/
+
+/*
  * PlainDraggable
  * https://anseki.github.io/plain-draggable/
  *
--- webpack.config.rules.js
+++ webpack.config.rules.js
@@ -36,9 +36,12 @@ module.exports = [
             if (LIMIT) {
               content = preProc.removeTag(LIMIT_TAGS, content);
               if (!BUILD && SRC && this.resourcePath === ENTRY_PATH) {
-                // Save the source code of limited function, to check.
+                // Save the source code of limited functions.
                 const destPath = path.resolve(SRC_PATH, BUILD_FILE);
-                require('fs').writeFileSync(destPath, content);
+                require('fs').writeFileSync(destPath,
+                  '/*\n  DON\'T MANUALLY EDIT THIS FILE; run ' +
+                  '`npm run dev-limit`' + // NPM command
+                  ` instead.\n*/\n\n${content}`);
                 console.log(`Output: ${destPath}`);
               }
             }
