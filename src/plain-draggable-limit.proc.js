ea296ff499559de6af4e384286c5f701ba7c2db9
anseki <AnSeki.aff@gmail.com>
2018-06-10 17:56:44 +0900

    Add remove

--- src/plain-draggable-limit.proc.js
+++ src/plain-draggable-limit.proc.js
@@ -101,6 +101,8 @@ const pointerEvent = {};
     return handlerId;
   };
 
+  pointerEvent.unregStartHandler = handlerId => { delete startHandlers[handlerId]; };
+
   // Gecko, Trident pick drag-event of some elements such as img, a, etc.
   function dragstart(event) { event.preventDefault(); }
 
@@ -752,6 +754,14 @@ class PlainDraggable {
     if (!options.handle) { options.handle = element; }
 
     setOptions(props, options);
+  }
+
+  remove() {
+    const props = insProps[this._id];
+    this.disabled = true; // To restore
+    pointerEvent.removeStartHandler(props.options.handle, props.pointerEventHandlerId);
+    pointerEvent.unregStartHandler(props.pointerEventHandlerId);
+    delete insProps[this._id];
   }
 
   /**
