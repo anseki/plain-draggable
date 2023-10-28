6557eb39d949a84325104a2ced227389022c1a6e
anseki <AnSeki.aff@gmail.com>
2023-10-29 01:02:30 +0900

    Add `viewscale.js`

---
+++ js/viewscale.js
@@ -0,0 +1,19 @@
+/*
+  Emulate `user-scalable=no` that is ignored by desktop browsers.
+  NOTE: This is imperfect because it can't hook UA commands (e.g. menu items).
+*/
+
+window.addEventListener('keydown', (event) => {
+  if (event.isComposing || event.keyCode === 229) { return; }
+  if ((event.key === '+' || event.key === '-') && event.ctrlKey) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+});
+
+window.addEventListener('mousewheel', (event) => {
+  if (event.ctrlKey) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}, { passive: false });
